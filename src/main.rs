6d1e5d484b7930916f0514e4f5f6a7f2d8b8aabb
Jaeyong Sung <jaeyong0201@gmail.com>
2022-01-21 16:28:14 +0900

    make simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let api = warp::path!("api" / "v1").map(|| "Not Implemented");
+    let file = warp::fs::dir("./public/dist");
+    let routes = warp::get().and(api.or(file));
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
